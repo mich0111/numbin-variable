3c19802defb7cf57c3a7d1ad88aeb852c49e0058
mich0111 <54777712+mich0111@users.noreply.github.com>
2019-12-06 11:42:54 +0100

    Cahngement de la tailled de la vignette

--- numbin-variable.js
+++ numbin-variable.js
@@ -1,4 +1,4 @@
-<div style="padding:0px; margin:2px 2px; width:85px; height:85px;" class="cmd #history# container-fluid tooltips cmd-widget" data-type="info" data-version="#version#" data-eqLogic_id="#eqLogic_id#" data-subtype="numeric" data-cmd_id="#id#" data-cmd_uid="#uid#">
+<div style="padding:0px; margin:2px 2px; width:80px; height:80px;" class="cmd #history# container-fluid tooltips cmd-widget" data-type="info" data-version="#version#" data-eqLogic_id="#eqLogic_id#" data-subtype="numeric" data-cmd_id="#id#" data-cmd_uid="#uid#">
  	<img class="background#uid#"/>
  	<img class="banner#uid#"/>
 	<img class="icon#uid#" style="transform:translate(-50%,-50%);"/>
@@ -158,8 +158,8 @@
 			vertical-align:middle;
 			top:82%;
 			left:0%;
-			width:85px;
-			height:85px;
+			width:80px;
+			height:80px;
 			z-index:4;
 		}
 		img.icon#uid# {
@@ -175,16 +175,16 @@
 			position:absolute;
 			top:0%;
 			left:0%;
-			width:85px;
-			height:85px;
+			width:80px;
+			height:80px;
 			z-index:2;
 		}
 		img.background#uid# {
 			position:absolute;
 			top:0%;
 			left:0%;
-			width:85px;
-			height:85px;
+			width:80px;
+			height:80px;
 			z-index:1;
 		}
 	</style>
