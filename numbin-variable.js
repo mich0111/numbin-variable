b7e49e0b29cfd7f87ec0713033244f9e6363de81
mich0111 <54777712+mich0111@users.noreply.github.com>
2019-12-14 15:03:15 +0100

    Allègement du code

--- numbin-variable.js
+++ numbin-variable.js
@@ -1,18 +1,18 @@
 <div style="padding:0px; margin:2px 2px; width:80px; height:80px;" class="cmd #history# container-fluid tooltips cmd-widget" data-type="info" data-version="#version#" data-eqLogic_id="#eqLogic_id#" data-subtype="numeric" data-cmd_id="#id#" data-cmd_uid="#uid#">
- 	<img class="background#uid#"/>
- 	<img class="banner#uid#"/>
+	<img class="background#uid#"/>
+	<img class="banner#uid#"/>
 	<img class="icon#uid#" style="transform:translate(-50%,-50%);"/>
 	<div class="txtban#uid#"/>
- 
+
 	<script>
 		jeedom.cmd.update['#id#'] = function(_options) {
 			// Récupération de srcState
 			var srcState = _options.display_value;	// Valeur de l'info binaire
 
 			// Récupération des valeurs des paramètres du widget
- 			var fldIcon = ('#folder#'!='#'+'folder#') ? '#folder#' : "";
+			var fldIcon = ('#folder#'!='#'+'folder#') ? '#folder#' : "";
 													// Dossier de l'image à superposer (obligatoire)
- 			var srcIcon = ('#icon#'!='#'+'icon#') ? '#icon#' : "";
+			var srcIcon = ('#icon#'!='#'+'icon#') ? '#icon#' : "";
 													// Image à superposer (obligatoire)
 			var srcTxtBanOn = ('#txtbanon#'!='#'+'txtbanon#') ? '#txtbanon#': "";
 													// Texte du bandeau ON (obligatoire)
@@ -39,11 +39,10 @@
 			var fldBkg = 'data/customTemplates/dashboard/cmd.action.other.Multi-action-Defaut/fond/';
 													// Dossier des images de background
 			var srcMode = "light";					// Mode du background (dark ou light)
-			var srcColBanner = "lime";				// Couleur du bandeau
-			var srcTxtBanner = "ON";				// Texte du bandeau
-			var srcColTxtBanner = "black";			// Couleur des caractères du bandeau
-			var srcBlink = false;					// Booléen de clignotement
-			var srcMode = "light";					// Mode du background (dark ou light)
+			var srcColBanner = "";					// Couleur du bandeau
+			var srcTxtBanner = "";					// Texte du bandeau
+			var srcColTxtBanner = "";				// Couleur des caractères du bandeau
+			var srcBlink = "";						// Clignotement
 			var srcErrorCode = "";					// Nom du paramètre en erreur s'il y a lieu
 		
 			// Validation des paramètres
@@ -78,47 +77,30 @@
 
 				// Initialisation de la couleur du bandeau et des caractères de la valeur
 				if (srcState == 0) {
-					if (srcTxtBanOff != "" && srcTxtBanOff != null) {
-						srcTxtBanner = srcTxtBanOff;
-					} else {
-						srcTxtBanner = "OFF";
-					}
-					if (srcColBanOff != "" && srcColBanOff != null) {
-						srcColBanner = srcColBanOff;
-					} else {
-						srcColBanner = "red";
-					}
-					if (srcColTxtBanOff != "" && srcColTxtBanOff != null) {
-						srcColTxtBanner = srcColTxtBanOff;
-					}
-					if (srcBlinkOff == 'yes') {
-						srcBlink = true;
-					}
+					srcTxtBanner = ((srcTxtBanOff != "") && (srcTxtBanOff != null)) ? srcTxtBanOff : "OFF";
+					srcColBanner = ((srcColBanOff != "") && (srcColBanOff != null)) ? srcColBanOff : "red";
+					srcColTxtBanner = ((srcColTxtBanOff != "") && (srcColTxtBanOff != null)) ? srcColTxtBanOff : "black";
+					srcIcon = (srcOnOff != "no") ? srcIcon + '_off.png' : srcIcon + '.png';
+					srcBlink = (srcBlinkOff == "yes") ? srcBlinkOff : "no";
 				} else {
-					if (srcTxtBanOn != "" && srcTxtBanOn != null) {
-						srcTxtBanner = srcTxtBanOn;
-					}
-					if (srcColBanOn != "" && srcColBanOn != null) {
-						srcColBanner = srcColBanOn;
-					}
-					if (srcColTxtBanOn != "" && srcColTxtBanOn != null) {
-						srcColTxtBanner = srcColTxtBanOn;
-					}
-					if (srcBlinkOn == 'yes') {
-						srcBlink = true;
-					}
+					srcTxtBanner = ((srcTxtBanOn != "") && (srcTxtBanOn != null)) ? srcTxtBanOn : "ON";
+					srcColBanner = ((srcColBanOn != "") && (srcColBanOn != null)) ? srcColBanOn : "lime";
+					srcColTxtBanner = ((srcColTxtBanOn != "") && (srcColTxtBanOn != null)) ? srcColTxtBanOn : "black";
+					srcIcon = (srcOnOff != "no") ? srcIcon + '_on.png' : srcIcon + '.png';
+					srcBlink = (srcBlinkOn == "yes") ? srcBlinkOn : "no";
 				}
 
 				//Affichage du background, du bandeau et du nom de la commande
 				if (srcTheme != "") {
 					srcTheme = srcTheme + '_';
-                }
+				}
 				// Affichage du fond
 				$('.background#uid#').empty().attr("src", fldBkg + 'fo_bkg_' + srcTheme + srcMode + '.png');
 
 				// Affichage du bandeau et de ses options
 				$('.banner#uid#').css('background-color',srcColBanner);
-				if (srcBlink) {					// Clignotement
+				if (srcBlink == 'yes') {					
+					// Clignotement
 					$('.banner#uid#').addClass('blinking');
 				}
 				else {
@@ -131,17 +113,7 @@
 				$('.txtban#uid#').empty().text(srcTxtBanner);
 
 				// Affichage de l'icone et de ses options
-				if (srcOnOff != "no") {
-					if (srcState == 0) {
-						$('.icon#uid#').empty().attr('src', fldIcon + srcIcon + '_off.png');
-					} else {
-						$('.icon#uid#').empty().attr('src', fldIcon + srcIcon + '_on.png');
-					}
-				}
-				 else {
-					$('.icon#uid#').empty().attr('src', fldIcon + srcIcon + '.png');
-				}
-
+				$('.icon#uid#').empty().attr('src', fldIcon + srcIcon);
 			}
 			$('.cmd[data-cmd_uid=#uid#]').attr('title','Valeur du '+_options.valueDate+', collectée le '+_options.collectDate);
 		}
